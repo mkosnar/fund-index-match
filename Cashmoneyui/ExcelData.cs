aaa5aff1956802ae8af1af14ec8575711ba0dae4
kosnamat <kosnamat@fel.cvut.cz>
2021-04-10 10:22:49 +0200

    Add project files.

---
+++ Cashmoneyui/ExcelData.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OfficeOpenXml;
+
+namespace Cashmoneyui
+{
+    class ExcelData
+    {
+        public ExcelData(string filePath)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            ep = new ExcelPackage(new FileInfo(filePath));
+            Errors = new List<ExcelDataError>();
+            rates = GetRates();
+            GetIndexData();
+        }
+
+        public decimal GetRate(DateTime day)
+        {
+            return rates[day];
+        }
+
+        ExcelPackage ep;
+        SortedDictionary<DateTime, decimal> rates;
+        List<ExcelDataError> Errors;
+        Dictionary<string, Dictionary<DateTime, decimal>> indexData;
+
+        private SortedDictionary<DateTime, decimal> GetRates()
+        {
+            var ret = new SortedDictionary<DateTime, decimal>();
+            var missing = new HashSet<DateTime>();
+
+            var ws = ep.Workbook.Worksheets["Kurz dolaru"];
+            var firstRow = ws.Dimension.Start.Row + 1;
+            var lastRow = ws.Dimension.End.Row;
+            for(var row = firstRow;row < lastRow;++row)
+            {
+                var cellRate = ws.Cells[row, 3];
+                var date = DateTime.Parse(ws.Cells[row, 2].Text);
+                var rateText = cellRate.Text;
+                decimal rate;
+                try
+                {
+                    rate = decimal.Parse(rateText);
+                }
+                catch (FormatException)
+                {
+                    Errors.Add(new ExcelDataError(ws.Name, cellRate.Address, row, 3, ExcelDataErrorType.ExpectedNumeric));
+                    rate = default;
+                }
+                if (rate != default)
+                    ret[date] = rate;
+                else
+                    missing.Add(date);
+            }
+
+            foreach(var date in missing)
+            {
+                var replacementDate = date.AddDays(-1);
+                while (!ret.ContainsKey(replacementDate))
+                    replacementDate = replacementDate.AddDays(-1);
+                ret[date] = ret[replacementDate];
+            }
+
+            return ret;
+        }
+
+        private Dictionary<DateTime, decimal> GetData(ExcelWorksheet ews, int colFrom)
+        {
+            var ret = new Dictionary<DateTime, decimal>();
+
+            int colDate = colFrom;
+            int colValue = colFrom + 1;
+            var firstRow = ews.Dimension.Start.Row + 1;
+            var lastRow = ews.Dimension.End.Row;
+
+            for(var row = firstRow;row < lastRow;++row)
+            {
+                var cellDate = ews.Cells[row, colDate];
+                var cellValue = ews.Cells[row, colValue];
+                if (string.IsNullOrEmpty(cellDate.Text))
+                    break;
+
+                var date = DateTime.Parse(cellDate.Text);
+                decimal value;
+                try
+                {
+                    value = decimal.Parse(cellValue.Text);
+                }
+                catch(FormatException)
+                {
+                    Errors.Add(new ExcelDataError(ews.Name, cellValue.Address, row, colValue, ExcelDataErrorType.ExpectedNumeric));
+                    value = default;
+                }
+                if (value != default)
+                    ret[date] = value;
+            }
+
+            return ret;
+        }
+
+        private void GetIndexData()
+        {
+            indexData = new Dictionary<string, Dictionary<DateTime, decimal>>();
+            (string, int)[] sheets = {("Top stocks a Nasdaq 10Y", 6), ("Top Stock a SP 10Y", 7), ("Top Stock a DJ 10Y", 8)};
+            foreach(var (sheet, col) in sheets)
+            {
+                indexData[sheet] = GetData(ep.Workbook.Worksheets[sheet], col);
+            }
+        }
+    }
+
+    class ExcelDataError
+    {
+        public ExcelDataError(string sheet, string addr, int row, int col, ExcelDataErrorType type)
+        {
+            Sheet = sheet;
+            Address = addr;
+            Row = row;
+            Column = col;
+            Type = type;
+        }
+
+        string Address { get; }
+        int Row { get; }
+        int Column { get; }
+        string Sheet { get; }
+        ExcelDataErrorType Type { get; }
+    }
+
+    enum ExcelDataErrorType
+    {
+        ExpectedNumeric,
+        MissingIndexValue,
+        Other
+    }
+}
---
+++ Cashmoneyui/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace Cashmoneyui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string sFileName;
+        ExcelData ed;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Excel File to Edit";
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Excel File|*.xlsx;*.xls";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                sFileName = openFileDialog1.FileName;
+            }
+
+            if (sFileName is not null && sFileName != "")
+            {
+                ed = new ExcelData(sFileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sDate = textBox2.Text;
+            
+            try
+            {
+                var day = DateTime.Parse(sDate);
+                textBox1.Text = ed.GetRate(day).ToString();
+            }
+            catch(Exception exc)
+            {
+                textBox1.Text = exc.ToString();
+            }
+        }
+    }
+}
